396d50c1b65b9022fcf509ddf8ae6c9b1aa8454f
Tiago <tgbahiense@gmail.com>
2024-12-03 00:46:24 -0300

    att class Emprestimos

---
+++ main/domain/model/Emprestimo.cs
@@ -0,0 +1,45 @@
+public class Emprestimo {
+    private int _IdInventario;
+    private int _IdCliente;
+    private DateTime _DataEmprestimo;
+    private DateTime _DataDevolucao;
+
+    public Emprestimo (int idInventario, int idCliente, DateTime dataEmprestimo, DateTime dataDevolucao){
+        IdInventario = idInventario;
+        IdCliente = idCliente;
+        DataEmprestimo = dataEmprestimo;
+        DataDevolucao = dataDevolucao;
+    }
+
+    public int IdInventario{
+        get {return _IdInventario;}
+        private set {_IdInventario = value;}
+    }
+
+    public int IdCliente{
+        get {return _IdCliente;}
+        private set {_IdCliente = value;}
+    }
+
+    public DateTime DataEmprestimo{
+        get {return _DataEmprestimo;}
+        set{
+            DateTime dataAtual = DateTime.Now;
+            if( value < dataAtual){
+                throw new ArgumentException("A data não pode ser anterior a data atual.")
+            }
+            _DataEmprestimo = value;
+        }
+    }
+
+    public DateTime DataDevolucao{
+        get {return _DataDevolucao;}
+        set {
+            DateTime dataAtual = DateTime.Now;
+            if(value <= dataAtual){
+                throw new ArgumentException("A data de devolução não pode ser menor ou igual a data atual.")
+            }
+            _DataDevolucao = value;
+        }
+    }
+}
